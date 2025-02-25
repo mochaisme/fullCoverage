36c54c455daab0cf7c698428beedd0b9ccc3c92d
T. Mochamad Rafly <trafly76@gmail.com>
2025-02-25 20:12:23 +0700

    Fix test_calculator

--- utests/test_calculator.py
+++ utests/test_calculator.py
@@ -1,28 +1,29 @@
 import pytest
 import sys
+import os
 
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../main')))
-import calculator
+import calculator as calc
 
 
 #import calculator 
 
 
 def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
+    assert calc.add(2, 3) == 5
+    assert calc.add(-1, 1) == 0
 
 def test_subtract():
-    assert subtract(10, 5) == 5
-    assert subtract(0, 7) == -7
+    assert calc.subtract(10, 5) == 5
+    assert calc.subtract(0, 7) == -7
 
 def test_multiply():
-    assert multiply(3, 4) == 12
-    assert multiply(-2, 3) == -6
+    assert calc.multiply(3, 4) == 12
+    assert calc.multiply(-2, 3) == -6
 
 def test_divide():
-    assert divide(10, 2) == 5
-    assert divide(9, 3) == 3
+    assert calc.divide(10, 2) == 5
+    assert calc.divide(9, 3) == 3
     with pytest.raises(ValueError):
-        divide(5, 0)
+        calc.divide(5, 0)
